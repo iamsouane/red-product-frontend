1c1193fadde0af5ced2a6e2cdb9ec9fa2f70e48b
iamsouane <ismailasouane08@gmail.com>
2025-06-17 13:35:09 +0000

    debut projet

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // ✅ active le support styled-components avec SWC
+  },
 };
 
 export default nextConfig;
